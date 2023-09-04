ea04ff1237ce147e11e8f606a07816d9520812ef
learnjagah <learnjagah.institute@gmail.com>
2023-09-04 19:12:35 +0530

    Functionality Fix

--- Components/09-Carousel/script.js
+++ Components/09-Carousel/script.js
@@ -1,11 +1,11 @@
 const slides = document.getElementsByClassName('slide');
-console.log(slides);
 let currentSlide = 1;
 
 function moveSlides(){
     currentSlide = 1;
     Array.from(slides).forEach((el, i) => {
-        el.style.transform  = `transalateX(${i*100}%)`;
+        console.log(`transalateX(${i*100}%)`);
+        el.style.transform  = `translateX(${i*100}%)`;
     });
 }
 
@@ -16,7 +16,7 @@ function nextSlide(){
 
     Array.from(slides).forEach((el, i) =>{
         if(currentSlide == i){
-            el.style.transform = `translateX(${(i-currentSlide) * 100}%)`;
+            el.style.transform = `translateX(${(i-currentSlide)*100}%)`;
         }
     });
     currentSlide++;
@@ -29,7 +29,7 @@ function prevSlide(){
     }
     Array.from(slides).forEach((el, i) =>{
         if(currentSlide-1 == i){
-            el.style.transform = `translateX(${(i-currentSlide) * 100}%)`;
+            el.style.transform = `translateX(${(i-currentSlide)*100}%)`;
         }
     });
     currentSlide--;
